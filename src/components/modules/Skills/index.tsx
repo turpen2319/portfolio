5dc4c2af7cc3182b84c08711ff4ae418640d779b
turpen2319 <austin-turpen@outlook.com>
2022-05-05 22:07:34 -0700

    config auto deploy for vercel

--- src/components/modules/Skills/index.tsx
+++ src/components/modules/Skills/index.tsx
@@ -13,13 +13,13 @@ const Skills = () => {
     <Wrapper id="about">
       <SkillsWrapper as={Container}>
         <Thumbnail>
-          <Image src={skillsIllustration} alt="I’m Austin and I’m a MERN & Django developer!" />
+          <Image src={skillsIllustration} alt="I'm Austin and I'm a MERN & Django developer!" />
         </Thumbnail>
         <Details theme={theme}>
           <h1>More about me</h1>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry’s standard dummy.
+            industry's standard dummy.
           </p>
           <Link href="#contact" passHref>
             <Button as="a">Contact me</Button>

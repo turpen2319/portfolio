c3aa8220bc0cecfb6b726a6a898096ca1d116468
turpen2319 <austin-turpen@outlook.com>
2022-05-17 13:14:39 -0700

    resolve missing file

--- src/components/ui/theme/Footer/index.tsx
+++ src/components/ui/theme/Footer/index.tsx
@@ -1,60 +1,40 @@
 import Image from 'next/image';
 import Container from 'components/ui/Container';
 import social from './social.json';
-import TelegramIcon from 'assets/social/telegram.svg';
+
 import GithubIcon from 'assets/social/github.svg';
-import StackOverflowIcon from 'assets/social/stackoverflow.svg';
-import TwitterIcon from 'assets/social/twitter.svg';
+
 import { Wrapper, Flex, Links, Details } from './styles';
 
 const icons = {
-	Telegram: TelegramIcon,
-	Github: GithubIcon,
-	StackOverflow: StackOverflowIcon,
-	Twitter: TwitterIcon,
+  Github: GithubIcon,
 };
 
 const Footer = () => (
-	<Wrapper>
-		<Flex as={Container}>
-			<Details>
-				<h2>Austin Turpen</h2>
-				<span>
-					© All rights are reserved | {new Date().getFullYear()} | Made with{' '}
-					<span aria-label="love" role="img">
-						💖
-					</span>{' '}
-					by{' '}
-					<a
-						href="https://smakosh.com/?ref=portfolio-dev"
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						Smakosh
-					</a>
-				</span>
-			</Details>
-			<Links>
-				{social.map(({ id, name, link }) => (
-					<a
-						key={id}
-						href={link}
-						target="_blank"
-						rel="noopener noreferrer"
-						aria-label={`follow me on ${name}`}
-					>
-						<Image
-							width={24}
-							height={24}
-							layout="fixed"
-							src={icons[name]}
-							alt={name}
-						/>
-					</a>
-				))}
-			</Links>
-		</Flex>
-	</Wrapper>
+  <Wrapper>
+    <Flex as={Container}>
+      <Details>
+        <h2>Austin Turpen</h2>
+        <span>
+          © All rights are reserved | {new Date().getFullYear()} | Made with{' '}
+          <span aria-label="love" role="img">
+            💖
+          </span>{' '}
+          by{' '}
+          <a href="https://smakosh.com/?ref=portfolio-dev" rel="noopener noreferrer" target="_blank">
+            Smakosh
+          </a>
+        </span>
+      </Details>
+      <Links>
+        {social.map(({ id, name, link }) => (
+          <a key={id} href={link} target="_blank" rel="noopener noreferrer" aria-label={`follow me on ${name}`}>
+            <Image width={24} height={24} layout="fixed" src={icons[name]} alt={name} />
+          </a>
+        ))}
+      </Links>
+    </Flex>
+  </Wrapper>
 );
 
 export default Footer;
